d762cf0f2c0f93871a0e82e072cc5001fc693a25
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-21 23:42:29 +0530

    Daily Employee Wage using Switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,23 @@
 public class EmployeeWage
 {
+
+public static final int IS_FULL_TIME=1,IS_PART_TIME=2;
+ public static int dailywage(int empCheck)
+    {
+    switch(empCheck){
+    case 1:
+	        return 8*20;
+    case 2:
+	        return 4*20;
+    default:
+	        return 0;
+    }
+    }
 public static void main(String[] args)
 {
-int IS_FULL_TIME=1,IS_PART_TIME=2;
-int result;
-double empCheck=Math.floor(Math.random() *10) % 3;
-result=(empCheck==IS_FULL_TIME)?8*20:(empCheck==IS_PART_TIME)?4*20:0;
+    int result;
+int empCheck= (int)Math.floor(Math.random() *10) % 3;
+result=dailywage(empCheck);
 System.out.println("Daily wage of employee is: "+result);
 
 }
