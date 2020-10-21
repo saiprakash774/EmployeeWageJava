279f3a7c4cf37aad1fe1b61fb02fd684f5c5dea6
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-21 23:19:55 +0530

    welcome

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+public class EmployeeWage
+{
+public static void main(String[] args)
+{
+int IS_FULL_TIME=1;
+String result;
+double empCheck=Math.floor(Math.random() *10) % 2;
+result=(empCheck==IS_FULL_TIME)?"EMPLOYEE IS PRESENT":"EMPLOYEE IS ABSENT";
+System.out.println(result);
+}
+}
